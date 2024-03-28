b8e64b95ca34cfe366894ae18f2ac8ccfc668fb4
Agustin Correnti <agustin.correnti@gmail.com>
2024-03-28 17:00:15 -0300

    Palabras Palindromas

---
+++ palabras_palindrome.py
@@ -0,0 +1,64 @@
+import unittest
+
+from is_palindrome import is_palindrome
+
+def obtener_cantidad_de_palabras_palindrome(palabras):
+    cant = 0
+    for palabra in palabras:
+        resultado = is_palindrome(palabra)
+        if resultado == True:
+            cant+=1
+        else:
+            continue
+    print(" ¿Cuantas palabras contiene?: ", len(palabras))
+    print("Palindromos: ", cant)
+    return cant
+
+class TestCantidadDePalabrasPalindromes(unittest.TestCase):
+    def test_cantidad_de_palabras_palindromes_simple(self):
+        palabras = ["ala"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 1)
+
+    def test_cantidad_de_palabras_palindromes_con_2(self):
+        palabras = ["ala", "neuquen"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 2)
+
+    def test_cantidad_de_palabras_palindromes_con_3(self):
+        palabras = ["ala", "neuquen", "hola"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 2)
+
+    def test_cantidad_de_palabras_palindromes_con_4(self):
+        palabras = ["ala", "neuquen", "hola", "programacion"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 2)
+
+    def test_cantidad_de_palabras_palindromes_con_5(self):
+        palabras = ["ala", "neuquen", "hola", "programacion", "palap"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 3)
+
+    def test_cantidad_de_palabras_palindromes_complejo(self):
+        palabras = ["ala", "neuquen", "hola", "programacion", "palap", "neu  quen"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 4)
+
+    def test_cantidad_de_palabras_palindromes_complejo_2(self):
+        palabras = [
+            "ala",
+            "neuquen",
+            "hola",
+            "programacion",
+            "palap",
+            "neu  quen",
+            "agita falsos usos la fatiga",
+            "presidente de la camara de diputados: martin menem",
+			"A man, a plan, a canal - Panama"
+        ]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 6)
+
+if __name__ == '__main__':
+    unittest.main()
